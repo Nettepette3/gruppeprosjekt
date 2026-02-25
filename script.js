289919e0ddb798b1987e79c015fc589bafbb6aa5
Angelica1234567891 <angelica.solveig@icloud.com>
2026-02-25 13:32:16 +0100

    Lagde egen js side

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let kortstokk = []
+        const containerElm = document.getElementById("c2")
+        const farger = ['clubs', 'diamonds', 'hearts', 'spades']
+        for (const tall in farger) {
+            for (let i = 1; i <= 13; i++) {
+                const kortNavn = "kortstokk/" + i + "_of_" + tall + ".png"
+                korstokk.push(kortNavn)
+
+            }
+
+
+        }
+        for (let n = 0; n < 5; n++) {
+            // "Popper" ut det siste kortet fra kortstokken:
+
+            const kortBilde = document.createElement("img")
+            kortBilde.src = kortNavn
+            containerElm.appendChild(kortBilde)
+        }
