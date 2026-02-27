5e78b9d2883afe4c89405ab68624f57ea462dd1e
Nettepette3 <christian.berg.olsen30@gmail.com>
2026-02-27 11:19:21 +0100

    .

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 function trekkort() {
+    const kortplass = document.getElementById("randomkort")
     const random1 = Math.floor(Math.random() * 13) + 1
     const random2 = Math.floor(Math.random() * 3)
     let farger = ['clubs', 'diamonds', 'hearts', 'spaces']
     const farge = farger[random2]
-    const bilde = document.createElement("img")
-    bilde.src = "kortstokk/" + random1 + "_of_" + farge + ".png"
+    kortplass.src = "kortstokk/" + random1 + "_of_" + farge + ".png"
     console.log(bilde.src)
 
 }
