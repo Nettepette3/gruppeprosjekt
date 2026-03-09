d79c84e2f0cb61896eb7092777ba0f5678e0a42e
Nettepette3 <christian.berg.olsen30@gmail.com>
2026-03-09 08:22:35 +0100

    .

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
+let valgtkort = null
+let tidligerekort = []
+let random1 = null
+let random2 = null
 
 function trekkort() {
+    if (valgtkort) {
+        return
+    }
+
     const kortplass = document.getElementById("randomkort")
     const random1 = Math.floor(Math.random() * 13) + 1
-    const random2 = Math.floor(Math.random() * 3)
-    let farger = ['clubs', 'diamonds', 'hearts', 'spaces']
+    const random2 = Math.floor(Math.random() * 4)
+    
+    let farger = ['clubs', 'diamonds', 'hearts', 'spades']
     const farge = farger[random2]
     kortplass.src = "kortstokk/" + random1 + "_of_" + farge + ".png"
-    console.log(bilde.src)
+
+
+
+
 
 }
 
-let valgtkort = []
+
 function trykk() {
 
+    const valgt = document.getElementById("randomkort")
+    valgtkort = valgt.src
 
+}
+
+
+function leggtil(x) {
+    const rute = document.getElementsByClassName("r")[x]
+
+    if (!valgtkort) {
+        return
+    }
+
+    let kortID = random1 + "_of_" + random2
+    tidligerekort.push(kortID)
+
+    const img = document.createElement("img")
+    img.src = valgtkort
+
+    rute.appendChild(img)
+
+    valgtkort = null
+    trekkort()
 
 }
